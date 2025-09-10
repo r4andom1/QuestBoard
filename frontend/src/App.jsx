381c74cb2b889dd7c092372a155e764f8d47b4e7
Kim Petersson <kim.petersson30@hotmail.com>
2025-09-10 20:45:07 +0200

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./css/App.css"
+import {Routes, Route} from "react-router-dom"
+
+function App() {
+  return (
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<h1>Replace this with dashboard page</h1>}></Route>
+      </Routes>
+    </main>
+  )
+}
+
+export default App
