b3ded1e94e07bee0315f9814ab1e1e2bbc1f1377
Kim Petersson <kim.petersson30@hotmail.com>
2025-09-15 16:47:22 +0200

    refactored router functionality from main to seperate file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { RouterProvider } from "react-router-dom"
 
 import "./css/index.css"
-import App from './App.jsx'
-import Dashboard from './pages/Dashboard.jsx'
-import LogIn from './pages/LogIn.jsx'
-import ErrorPage from './pages/ErrorPage.jsx'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    errorElement: <ErrorPage/>
-  },
-  {
-    path: "/log-in",
-    element: <LogIn/>
-  }
-
-])
+import { router } from './router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
