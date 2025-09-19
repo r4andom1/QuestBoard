76e5b0777100829bf9216bdf1deb62d075bfa035
Kim Petersson <kim.petersson301@gmail.com>
2025-09-19 14:31:15 +0200

    added default value

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -9,7 +9,7 @@ function Task() {
   const [newTaskName, setNewTaskName] = useState("");
   const [taskList, setTaskList] = useState([]);
   const [newDescription, setNewDescription] = useState("");
-  const [newType, setNewType] = useState("");
+  const [newType, setNewType] = useState("one-time");
 
   const currentUserData = UserAuth().session.user;
 
