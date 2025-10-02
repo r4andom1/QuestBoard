01372e8a47681a521ecab886f8be76042a72d8b7
Kim Petersson <kim.petersson301@gmail.com>
2025-10-02 11:25:40 +0200

    adding userstats data to the dashboard

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,13 +1,48 @@
 import "../css/HeroSection.css";
+import { useState, useEffect } from "react";
+import supabase from "../../services/supabase-client";
+import { getCurrentUserData } from "../utils/getCurrentUser.js";
+import { Coins } from "lucide-react";
 
 export default function HeroSection() {
-  return (
-    <section className="hero-section">
-      <div>
-        <h1>This is the hero section!</h1>
-        <p>Here is some text about the user</p>
-        <p>maybe some stats</p>
-      </div>
-    </section>
-  );
+  const [userStats, setUserStats] = useState({});
+  const { currentUserID, currentUserData } = getCurrentUserData();
+  // const authContext = UserAuth();
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  const fetchUserData = async () => {
+    const { data, error } = await supabase
+      .from("user_stats")
+      .select("*")
+      .eq("user_id", currentUserID)
+      .single();
+    if (error) {
+      console.log("Error fetching user", error);
+    } else {
+      setUserStats(data);
+      //   console.log(data);
+    }
+  };
+
+  function userDetails(user) {
+    return (
+      <section className="hero-section">
+        <div>
+          <h1>Welcome back, user!</h1>
+          <p>Here is some text about the user</p>
+          <p>Level {user.level}</p>
+          <p>XP: {user.current_xp}</p>
+          <p>
+            <Coins /> {user.coins}
+          </p>
+          <img></img>
+        </div>
+      </section>
+    );
+  }
+
+  return userDetails(userStats);
 }
